a1fd7c65c07816e91f51354980763b96951b8cc0
APKakuja <adrianpetrisorkakuja@gmail.com>
2025-09-19 19:18:22 +0200

    first commit from first class

---
+++ 01-Rot13/src/Rot13.java
@@ -0,0 +1,23 @@
+import java.lang.classfile.instruction.ReturnInstruction;
+import java.util.Scanner;
+
+
+public class Rot13 {
+
+
+char[] minusculas = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+char[] maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public String xifraRot13(String input) {
+
+ 
+    return null;
+
+ 
+    } 
+    
+    public String decifraRot13(String input) {
+    
+    return  xifraRot13(input);
+    }
+}
