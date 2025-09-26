93665dc1906454b4a93737a9bf2a5d73e4191b97
APKakuja <adrianpetrisorkakuja@gmail.com>
2025-09-26 17:01:16 +0200

    Commit inicial segon Ex2

---
+++ 02-RotX i força bruta/src/RotX.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class RotX {
+
+static char[] minusculas = "aàábcçdeèéfghiíïjklmnñoòópqrstuúüvwxyz".toCharArray();
+static char[] maiusculas = "AÀÁBCÇDEÈÉFGHIÍÏJKLMNÑOÒÓPQRSTUÚÜVWXYZ".toCharArray();
+public static void main(String[] args) {
+Scanner prova = new Scanner(System.in);
+        String text = "";
+        String xifrat = "";
+        String desxifrat = " ";
+
+        System.out.println("Introdueix un text per xifrar:");
+        text = prova.nextLine();
+
+
+        // xifrat = xifraRotX(text);
+        //System.out.println(xifrat + " text cifrat");
+        // desxifrat = desxifraRotX(xifrat);
+        // System.out.println(desxifrat + " text descifrat");
+
+}
+
+
+
+}
+
+
