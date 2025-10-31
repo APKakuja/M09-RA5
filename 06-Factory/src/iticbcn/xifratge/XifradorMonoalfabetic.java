27ff7f4d0a4121497669339f24dc23c992a41b56
APKakuja <adrianpetrisorkakuja@gmail.com>
2025-10-31 17:26:54 +0100

    Commit final de clase

---
+++ 06-Factory/src/iticbcn/xifratge/XifradorMonoalfabetic.java
@@ -0,0 +1,97 @@
+package iticbcn.xifratge;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class XifradorMonoalfabetic {
+static char[] maiusculas = "AÀÁBCÇDEÈÉFGHIÍÏJKLMNÑOÒÓPQRSTUÚÜVWXYZ".toCharArray();
+
+    // Array amb la permutació
+    private static char[] permutacio;
+
+    public static void main(String[] args) {
+        // Generar i mostras la permutació
+        permutacio = permutaAlfabet(maiusculas);
+
+        System.out.println("Alfabet original: " + String.valueOf(maiusculas));
+        System.out.println("Alfabet permutat: " + String.valueOf(permutacio));
+
+        // Exemples
+        String[] ejemplos = {
+            "ABC",
+            "XYZ",
+            "Hola, Mr. calçot",
+            "Perdó, per tu què és?"
+        };
+
+        for (String text : ejemplos) {
+            String xifrat = xifraMonoAlfa(text);
+            String desxifrat = desxifraMonoAlfa(xifrat);
+
+            System.out.println("\nText original:   " + text);
+            System.out.println("Text xifrat:     " + xifrat);
+            System.out.println("Text desxifrat:  " + desxifrat);
+        }
+    }
+
+    // Genera la permutació de l'alfabet
+    public static char[] permutaAlfabet(char[] alfabet) {
+        List<Character> llista = new ArrayList<>();
+        for (char c : alfabet) {
+            llista.add(c);
+        }
+
+        Collections.shuffle(llista); // Barreja de manera aleatoria
+
+        char[] resultat = new char[alfabet.length];
+        for (int i = 0; i < alfabet.length; i++) {
+            resultat[i] = llista.get(i);
+        }
+        return resultat;
+    }
+
+    // Xifra el text
+    public static String xifraMonoAlfa(String cadena) {
+        StringBuilder resultat = new StringBuilder();
+
+        for (char c : cadena.toCharArray()) {
+            resultat.append(substitueix(c, maiusculas, permutacio));
+        }
+
+        return resultat.toString();
+    }
+
+    // Desxifra el text
+    public static String desxifraMonoAlfa(String cadena) {
+        StringBuilder resultat = new StringBuilder();
+
+        for (char c : cadena.toCharArray()) {
+            resultat.append(substitueix(c, permutacio, maiusculas));
+        }
+
+        return resultat.toString();
+    }
+
+    private static char substitueix(char c, char[] origen, char[] desti) {
+        boolean esMinuscula = Character.isLowerCase(c);
+        char majuscula = Character.toUpperCase(c);
+
+        // Busca el character en el array origen
+        for (int i = 0; i < origen.length; i++) {
+            if (origen[i] == majuscula) {
+                char substitut = desti[i];
+
+                // Si esta en minuscula retorna en maiuscula
+                if (esMinuscula) {
+                    return Character.toLowerCase(substitut);
+                } else {
+                    return substitut;
+                }
+            }
+        }
+
+        // Si no es una lletra es retorna igual
+        return c;
+    }
+}
---
+++ 06-Factory/src/iticbcn/xifratge/XifradorRotX.java
@@ -0,0 +1,141 @@
+package iticbcn.xifratge;
+import java.util.Scanner;
+
+public class XifradorRotX {
+static char[] minusculas = "aàábcçdeèéfghiíïjklmnñoòópqrstuúüvwxyz".toCharArray();
+    static char[] maiusculas = "AÀÁBCÇDEÈÉFGHIÍÏJKLMNÑOÒÓPQRSTUÚÜVWXYZ".toCharArray();
+
+    public static void main(String[] args) {
+        Scanner prova = new Scanner(System.in);
+        String text;
+        String xifrat;
+        String desxifrat;
+        String[] ejemplos = {
+                "ABC",
+                "XYZ",
+                "Hola, Mr. calçot",
+                "Perdó, per tu què és?"
+        };
+
+        //Desplazamientos per casos de proba automatic, tal com s'indica al anunciat
+        int[] desplazamientos = {0, 2, 4, 6};
+
+
+        System.out.println("Xifrat");
+        System.out.println("------");
+        for (int i = 0; i < ejemplos.length; i++) {
+            text = ejemplos[i];
+            int d = desplazamientos[i];
+            xifrat = xifraRotX(text, d);
+            System.out.println("(" + d + ")-" + text + " => " + xifrat);
+        }
+
+        System.out.println("Desxifrat");
+        System.out.println("---------");
+        for (int i = 0; i < ejemplos.length; i++) {
+            xifrat = xifraRotX(ejemplos[i], desplazamientos[i]);
+            desxifrat = desxifraRotX(xifrat, desplazamientos[i]);
+            System.out.println("(" + desplazamientos[i] + ")-" + xifrat + " => " + desxifrat);
+        }
+
+        xifrat = xifraRotX("Perdó, per tu què és?", 6);
+        System.out.println("\nMissatge xifrat: " + xifrat);
+        System.out.println("----------------");
+
+        //Utilitza el array minusculas per els numeros de desplazament totals
+        for (int g = 0; g < minusculas.length; g++) {
+            desxifrat = desxifraRotX(xifrat, g);
+            System.out.println("(" + g + ")->" + desxifrat);
+        }
+
+        // Demanem casos de prova
+        System.out.println("\nIntrodueix un text per xifrar:");
+        text = prova.nextLine();
+
+        // Demanem el desplazamiento
+        System.out.println("Introdueix el desplaçament (nombre enter):");
+        int desplazamiento = prova.nextInt();
+
+        // Xifra el text
+        xifrat = xifraRotX(text, desplazamiento);
+        System.out.println("\nText xifrat: " + xifrat);
+
+        // Desxifra el text
+        desxifrat = desxifraRotX(xifrat, desplazamiento);
+        System.out.println("Text desxifrat: " + desxifrat);
+
+        // Probem la força brutax
+        System.out.println("\nForça bruta:");
+        forcaBrutaRotX(xifrat);
+    }
+
+    // Funció para xifrar amb desplazamiento variable
+    public static String xifraRotX(String text, int desplazamiento) {
+        String PalabraNueva = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    for (int j = 0; j < minusculas.length; j++) {
+                        if (minusculas[j] == c) {
+                            int lugar = (j + desplazamiento) % minusculas.length;
+                            PalabraNueva += minusculas[lugar];
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < maiusculas.length; j++) {
+                        if (maiusculas[j] == c) {
+                            int lugar = (j + desplazamiento) % maiusculas.length;
+                            PalabraNueva += maiusculas[lugar];
+                        }
+                    }
+                }
+            } else {
+                PalabraNueva += c; // Si no son lletres es mantenen igual
+            }
+        }
+        return PalabraNueva;
+    }
+
+    // Funció per desxifrar amb desplazamiento variable
+    public static String desxifraRotX(String text, int desplazamiento) {
+        String PalabraNueva = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    for (int j = 0; j < minusculas.length; j++) {
+                        if (minusculas[j] == c) {
+                            int lugar = (j - desplazamiento) % minusculas.length;
+                            if (lugar < 0) lugar += minusculas.length;
+                            PalabraNueva += minusculas[lugar];
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < maiusculas.length; j++) {
+                        if (maiusculas[j] == c) {
+                            int lugar = (j - desplazamiento) % maiusculas.length;
+                            if (lugar < 0) lugar += maiusculas.length;
+                            PalabraNueva += maiusculas[lugar];
+                        }
+                    }
+                }
+            } else {
+                PalabraNueva += c;
+            }
+        }
+        return PalabraNueva;
+    }
+
+    // Funció de força brutaX
+    public static void forcaBrutaRotX(String text) {
+        for (int d = 1; d <= minusculas.length; d++) {
+            String intento = desxifraRotX(text, d);
+            System.out.println("(" + d + ") -> " + intento);
+        }
+    }
+}
