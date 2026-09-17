a0b313058a8acb0fdb3092e8baf1edbe584fb2e6
APKakuja <adrianpetrisorkakuja@gmail.com>
2026-09-17 18:58:45 +0200

    Commit final de classe 01-Rot13

---
+++ 01-Rot13/src/Rot13.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Rot13 {
+
+    static char[] majuscules = "aàábcçdeèéfghiíïjklmnñoòópqrstuúüvwxyz".toCharArray();
+    static char[] minuscules = "aàábcçdeèéfghiíïjklmnñoòópqrstuúüvwxyz".toCharArray(); 
+
+    public static void main(String[] args) {
+        Scanner paraula = new Scanner(System.in);
+
+        String text = "";
+        String textXifrat = "";
+        String textDesxifrat = "";
+
+        System.out.println("Introdueix text per xifrar");
+
+    }
+
+    static void xifraRot13(String text) {
+
+        int llarg = text.length();
+        int condicio = llarg;
+
+        char c = ' ';
+
+    }
+
+    static void desxifraRot13(String text) {
+
+
+    }
+}
