d9b7602ecf712b407722ab3a13eb7b4ba1d76b49
APKakuja <adrianpetrisorkakuja@gmail.com>
2026-09-25 18:55:37 +0200

    02-RotX i força bruta, casi acabat falten millores per complir al 100% amb l'enunciat

--- 02-RotX i força bruta/src/RotX.java
+++ 02-RotX i força bruta/src/RotX.java
@@ -17,18 +17,103 @@ public class RotX {
                 "Perdó, per tu què és?"
         };
 
+        int[] desplazamientos = {0, 2, 4, 6};
+
+        System.out.println("Xifrat");
+
+
+        for (int i = 0; i < ejemplos.length; i++) {
+
+            text = ejemplos[i];
+            int d = desplazamientos[i];
+            xifrat = xifraRotX(text, d);
+            System.out.println("(" + d + ")-" + text + " => " + xifrat);
+
+        }
+
+        System.out.println("Desxifrat");
+
+        for (int i = 0; i < 10; i++) {
+            xifrat = xifraRotX(ejemplos[i], desplazamientos[i]);
+            desxifrat = desxifraRotX(xifrat, desplazamientos[i]);
+
+            System.out.println("(" + desplazamientos[i] + ")-" + xifrat + " => " + desxifrat);
+
+        }
+
+        xifrat = xifraRotX("Perdó, per tu què és?", 6);
+        System.out.println("\nMissatge xifrat: " + xifrat);
+
+        for (int g = 0; g < minuscules.length; g++) {
+            desxifrat = desxifraRotX(xifrat, g);
+            System.out.println("(" + g + ")->" + desxifrat);
+        }
     }
 
-     public static String xifraRotX(String text) {
+     public static String xifraRotX(String text, int desplazamiento) {
         String PalabraNueva = "";
 
-        return PalabraNueva;
-     }
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
 
-     public static String desxifraRotX(String text) {
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    for (int j = 0; j < minuscules.length; j++) {
+                        if (minuscules[j] == c) {
+                            int lugar = (j + desplazamiento) % minuscules.length;
+                            PalabraNueva += minuscules[lugar];
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < majuscules.length; j++) {
+                        if (majuscules[j] == c) {
+                            int lugar = (j + desplazamiento) % majuscules.length;
+                            PalabraNueva += majuscules[lugar];
+                        }
+                    }
+                }
+            } else {
+                PalabraNueva += c;
+            }
+        }
+        return PalabraNueva;
+    }
+
+    public static String desxifraRotX(String text, int desplazamiento) {
         String PalabraNueva = "";
 
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    for (int j = 0; j < minuscules.length; j++) {
+                        if (minuscules[j] == c) {
+                            int lugar = (j - desplazamiento) % minuscules.length;
+                            if (lugar < 0) lugar += minuscules.length;
+                            PalabraNueva += minuscules[lugar];
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < majuscules.length; j++) {
+                        if (majuscules[j] == c) {
+                            int lugar = (j - desplazamiento) % majuscules.length;
+                            if (lugar < 0) lugar += majuscules.length;
+                            PalabraNueva += majuscules[lugar];
+                        }
+                    }
+                }
+            } else {
+                PalabraNueva += c;
+            }
+        }
         return PalabraNueva;
      }
-
+     
+      public static void forcaBrutaRotX(String text) {
+        for (int d = 1; d <= minuscules.length; d++) {
+            String intento = desxifraRotX(text, d);
+            System.out.println("(" + d + ") -> " + intento);
+        }
+    }
 }
